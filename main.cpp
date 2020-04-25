d6888397fc70165b57fd5d048720e33b855fa54e
Ekaterina-Kubrak <kubrakya@mpei.ru>
2020-04-25 22:16:16 +0300

    code:поменяла цвета гистограммы

--- main.cpp
+++ main.cpp
@@ -22,9 +22,9 @@ void svg_text(double left, double baseline, string text)
     cout << "<text x='" << left << "' y='"<<baseline<<"'>"<<text<<"</text>";
 }
 
-void svg_rect(double x, double y, double width, double height)
+void svg_rect(double x, double y, double width, double height, string stroke = "mediumorchid", string fill = "mediumorchid")
 {
-    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "'/>'";
+    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "'/>'";
 }
 
 void show_histogram_svg(const vector<size_t>& bins)

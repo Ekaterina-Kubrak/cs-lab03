9a7cbfe5a3d5658f0dc06e791ee9f4924fe4c734
Ekaterina-Kubrak <kubrakya@mpei.ru>
2020-04-25 23:59:00 +0300

    build:добавлены файлы histogram.h и histogram.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "histogram.h"
 #include <iostream>
 #include <vector>
 
@@ -56,23 +57,6 @@ vector<double>input_numbers(size_t count)
         cin >> result[i];
     }
     return result;
-}
-
-void find_minmax(const vector<double>& numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers)
-    {
-        if (number < min)
-        {
-            min = number;
-        }
-        if (number > max)
-        {
-            max = number;
-        }
-    }
 }
 
 vector<size_t> make_histogram(const vector<double>& numbers, size_t bin_count, double min, double max)

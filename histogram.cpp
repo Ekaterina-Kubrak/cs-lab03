737748de98b5d47bdcc05e02d4deeb513c17f89d
Ekaterina-Kubrak <kubrakya@mpei.ru>
2020-04-27 11:53:39 +0300

    code:добавила тесты в один файл

--- histogram.cpp
+++ histogram.cpp
@@ -2,17 +2,20 @@
 #include <iostream>
 void find_minmax(const vector<double>& numbers, double& min, double& max)
 {
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers)
+    if(numbers.size() !=0)
     {
-        if (number < min)
+        min = numbers[0];
+        max = numbers[0];
+        for (double number : numbers)
         {
-            min = number;
-        }
-        if (number > max)
-        {
-            max = number;
+            if (number < min)
+            {
+                min = number;
+            }
+            if (number > max)
+            {
+                max = number;
+            }
         }
     }
 }
--- test.cpp
+++ test.cpp
@@ -11,7 +11,47 @@ void test_positive()
     assert(max == 3);
 }
 
+void test_1()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({-1, -2, -3}, min, max);
+    assert(min == -3);
+    assert(max == -1);
+}
+
+void test_2()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({2, 2, 2}, min, max);
+    assert(min == 2);
+    assert(max == 2);
+}
+
+void test_3()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({2}, min, max);
+    assert(min == 2);
+    assert(max == 2);
+}
+
+void test_4()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({}, min, max);
+    assert(min == 0);
+    assert(max == 0);
+}
+
 int main()
 {
     test_positive();
+    test_1();
+    test_2();
+    test_3();
+    test_4();
 }

af4fd146206e887153ec515dc133492e5678bd31
Ekaterina-Kubrak <kubrakya@mpei.ru>
2020-05-15 14:28:55 +0300

    code:сохранение данных из сети в буфер

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include "svg.h"
 #include <curl/curl.h>
+#include <sstream>
+#include <string>
 using namespace std;
 
 vector<double>input_numbers(istream& in, size_t count)
@@ -96,36 +98,51 @@ void show_histogram_text(vector<size_t>bins)
     }
 }
 
+size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+    size_t data_size = item_size * item_count;
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(reinterpret_cast<const char*>(items), data_size);
+    return data_size;
+}
+
+Input download(const string& address)
+{
+    stringstream buffer;
+    curl_global_init(CURL_GLOBAL_ALL);
+    CURL *curl = curl_easy_init();
+    if(curl)
+    {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK)
+        {
+            cout << curl_easy_strerror(res);
+            exit(1);
+        }
+        curl_easy_cleanup(curl);
+    }
+    return read_input(buffer, false);
+}
+
 int main(int argc, char* argv[])
 {
-    curl_global_init(CURL_GLOBAL_ALL);
+    Input input;
     if(argc > 1)
     {
-        CURL *curl = curl_easy_init();
-        if(curl)
-        {
-            CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-            res = curl_easy_perform(curl);
-            if(res != CURLE_OK)
-            {
-                cout << curl_easy_strerror(res);
-                exit(1);
-            }
-            curl_easy_cleanup(curl);
-        }
-        return 0;
+        input = download(argv[1]);
     }
-    // Ввод данных
-    const auto input = read_input(cin, true);
-    // Обработка данных
-
+    else
+    {
+        input = read_input(cin, true);
+    }
     double min, max;
     find_minmax(input, min, max);
 
     const auto bins = make_histogram(input, min, max);
-
-    // Вывод данных
 
     show_histogram_svg(bins);
 
